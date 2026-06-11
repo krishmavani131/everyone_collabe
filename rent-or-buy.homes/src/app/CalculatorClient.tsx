3aacafc568811e3ef6a29d06468081919b60421c
mili sheta <mili.we3vision@gmail.com>
2026-06-11 14:15:59 +0530

    Delete rent-or-buy.homes directory

--- rent-or-buy.homes/src/app/CalculatorClient.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import "../i18n/i18n";
-import App from "../App";
-
-export default function CalculatorClient() {
-  return <App />;
-}
--- rent-or-buy.homes/src/components/InputPanel/shared/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NumberInput } from './NumberInput';
-export { default as SliderInput } from './SliderInput';  
-export { default as ButtonGroup } from './ButtonGroup';
